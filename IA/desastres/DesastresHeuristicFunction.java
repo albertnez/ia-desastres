6e2b9f1f06ea7f86b50754388c5322b84c1b15fe
Guido Arnau <guido.arnau@gmail.com>
2014-10-12 17:01:32 +0200

    Fixed more indenting

--- IA/desastres/DesastresHeuristicFunction.java
+++ IA/desastres/DesastresHeuristicFunction.java
@@ -4,9 +4,9 @@ import aima.search.framework.HeuristicFunction;
 
 public class DesastresHeuristicFunction implements HeuristicFunction {
 
-    public double getHeuristicValue(Object state) {
-        DesastresState st = (DesastresState)state;
-        return st.getTypeASolutionCost();
-    }
+  public double getHeuristicValue(Object state) {
+    DesastresState st = (DesastresState)state;
+    return st.getTypeASolutionCost();
+  }
 
 }
