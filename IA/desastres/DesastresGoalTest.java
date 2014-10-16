e8dc8ae58ba88115c2c565eb930d82f4853aecce
Guido Arnau Antoniucci <guido.arnau@a5s104mac32.fib.upc.es>
2014-10-16 17:57:03 +0200

    Updated more docs

--- IA/desastres/DesastresGoalTest.java
+++ IA/desastres/DesastresGoalTest.java
@@ -4,6 +4,15 @@ import aima.search.framework.GoalTest;
 
 public class DesastresGoalTest implements GoalTest {
 
+  /*!\brief Returns true if the state aState is a valid
+  * final state, false otherwise
+  *
+  * Because this program is only being executed with SA
+  * and HC, the goal state function will always return false
+  *
+  * @param [Object] aState State of the problem
+  * @return boolean indicates if aState is a goal state
+  */
   public boolean isGoalState(Object aState) {
     return(false);
   }
